ce6610e9843e115d79215591692c4bdb833a4533
kng83 <pawel.kng@gmail.com>
2020-01-03 22:07:32 +0100

    some commit

--- src/Confirm.tsx
+++ src/Confirm.tsx
@@ -1,50 +1,63 @@
-import * as React from 'react';
+import * as React from "react";
 import "./Confirm.css";
 
-interface ConfirmProps{
-    title:string;
-    content:string;
-    cancelCaption?: string;
-    okCaption?: string;
-   
+interface ConfirmProps {
+  open: boolean;
+  title: string;
+  content: string;
+  cancelCaption?: string;
+  okCaption?: string;
+  onOkClick: () => void;
+  onCancelClick: () => void;
 }
 
 class Confirm extends React.Component<ConfirmProps> {
+  public static defaultProps = {
+    cancelCaption: "cancel",
+    okCaption: "ok"
+  };
 
-    public static defaultProps = {
-        cancelCaption : "cancel",
-        okCaption: "ok"
-    }
+  attachText() {
+    return "some other text";
+  }
 
-    attachText(){
-        return 'some other text';
-    }
+  private handleCancelClick = () => {
+    this.props.onCancelClick();
+  };
 
-    public render() {
-    //    const  props = {
-    //         title: "React and TypeScript",
-    //         otherText: this.attachText()
-    //        };
+  private handleOkClick = () => {
+    this.props.onOkClick();
+  };
 
-        return (
-            <div className="confirm-wrapper confirm-visible">
-                <div className="confirm-container">
-                    <div className="confirm-title-container">
-                        <span>{this.props.title}</span>
-                        <span>{this.props.content}</span>
-                    </div>
-                    <div className="confirm-content-container">
-                        <p>This is where our content should go</p>
-                    </div>
-                    <div className="confirm-buttons-container">
-                        <button className="confirm-cancel">{this.props.cancelCaption}</button>
-                        <button className="confirm-ok">{this.props.okCaption}</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  
 
+  public render() {
+    return (
+      <div className={ this.props.open ? "confirm-wrapper confirm-visible": "confirm-wrapper"}>
+        <div className="confirm-container">
+          <div className="confirm-title-container">
+            <span>{this.props.title}</span>
+            <span>{this.props.content}</span>
+          </div>
+          <div className="confirm-content-container">
+            <p>This is where our content should go</p>
+          </div>
+          <div className="confirm-buttons-container">
+            <button className="confirm-cancel" onClick={this.handleCancelClick}>
+              {this.props.cancelCaption}
+            </button>
+            <button className="confirm-ok" onClick={this.handleOkClick}>
+              {this.props.okCaption}{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+//A tak wygladalaby klasa confirm
+//let some = new Confirm({title:"some",content:"some content"});
+//console.log(some);
 
 export default Confirm;
