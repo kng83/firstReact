2246e0318706ca98df8a6f3017aa86d141a00c60
kng83 <pawel.kng@gmail.com>
2020-01-09 22:06:24 +0100

    some

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,12 +2,16 @@ import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
+import Header from './Header';
+import ProductPage from './ProductPage';
 
 const Routes: React.SFC = () => {
   return (
     <Router>
+      <Header/>
       <div>
         <Route path="/products" component={ProductsPage} />
+        <Route path="/products/:id" component={ProductPage} />
         <Route path="/admin" component={AdminPage} />
       </div>
     </Router>
